8115f506397b6f45a1b3d213998b7acfb1a00d56
OliverSaldivar <oliverg092004@gmail.com>
2023-11-08 00:32:44 -0600

    creado programas en java

---
+++ _p114_Animal/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J"); System.out.flush();
+        Gato migato = new Gato();
+        migato.sonido();
+        migato.dormir();
+    }
+}
